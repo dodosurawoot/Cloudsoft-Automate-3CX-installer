710d6dffd3b36f7951b3ac07252eeccfbee00770
Dodo_Surawwoot <dodosurawoot@outlook.com>
2022-07-05 04:19:20 +0700

    Update DodoAutomate3CXPBX.sh

--- DodoAutomate3CXPBX.sh
+++ DodoAutomate3CXPBX.sh
@@ -5,7 +5,7 @@ set -e
 
 #Update and inastall all the required packages
 apt-get update -y
-apt- upgrade -y
+apt-get upgrade -y
 
 #auto create file and fill test in file
 echo /etc/apt/sources.list.d/3cxpbx.list 
@@ -21,7 +21,7 @@ apt-key add public.key -y
 echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | tee /etc/apt/sources.list.d/3cxpbx.list
 
 #Update and inastall all the required packages
-apt-get updae -y
+apt-get update -y
 
 #remove locales
 apt-get purge locales -y
